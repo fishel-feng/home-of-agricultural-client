93585fcd376e5cbb276675605b8854e3110fc00e
fx109138 <1091381572@qq.com>
2017-12-09 21:17:26 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div id="app">
+  <mt-header fixed :title="selected"></mt-header>
+  <router-view></router-view>
+  <mt-tabbar v-model="selected">
+    <mt-tab-item id="新闻">
+      <img slot="icon" src="./assets/home.svg"> 新闻
+    </mt-tab-item>
+    <mt-tab-item id="百科">
+      <img slot="icon" src="./assets/wiki.svg"> 百科
+    </mt-tab-item>
+    <mt-tab-item id="专家">
+      <img slot="icon" src="./assets/question.svg"> 问专家
+    </mt-tab-item>
+    <mt-tab-item id="农友圈">
+      <img slot="icon" src="./assets/circle.svg"> 农友圈
+    </mt-tab-item>
+    <mt-tab-item id="我的">
+      <img slot="icon" src="./assets/me.svg"> 我的
+    </mt-tab-item>
+  </mt-tabbar>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      selected: '新闻'
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/News.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="news">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.news{
+  position: fixed;
+  width: 100%;
+  top: 40px;
+  bottom: 0;
+}
+</style>
